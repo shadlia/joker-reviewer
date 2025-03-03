05dc490b3710d31a0f631494ef91ce2ac043bf30
shadlia <shadliaelamri@gmail.com>
2025-03-03 15:17:15 +0100

    add reviewer agent

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ load_dotenv()
 class State(TypedDict):
     context: str
     joke: str
+    funny_or_not: bool
+    reason: str
 
 
 # Agent 1 : the joker
@@ -17,6 +19,7 @@ def joker_agent(state: State):
     prompt = ChatPromptTemplate.from_template(
         """
        - You are a joker. You are in a room with a reviewer. You are trying to make the reviewer laugh.
+       - Try to give dull and boring jokes sometimes .
        - Make sure its just a single joke.
        - This is the context of the joke : {context}"""
     )
@@ -28,19 +31,35 @@ def joker_agent(state: State):
 
 
 # Agent 2 : the reviewer
-def reviewer_agent():
-    return
+def reviewer_agent(state: State):
+    prompt = ChatPromptTemplate.from_template(
+        """
+         - You are a reviewer. You are in a room with a joker. You are trying to not laugh at the joker's joke.
+         - This is the context of the joke : {context} and this is the given joke : {joke}
+         - Be strict and hard to laugh at the joker's joke.
+         - if the joke is funny return True otherwise return False.
+     
+         
+    """
+    )
+    model = ChatOpenAI(model="gpt-4o-mini")
+    chain = prompt | model
+    response = chain.invoke({"context": state["context"], "joke": state["joke"]})
+    print(response.content)
+    return {"funny_or_not": response.content}
 
 
 def build_graph():
     graph = StateGraph(State)
     graph.add_node("joker_agent", joker_agent)
     graph.set_entry_point("joker_agent")
-    graph.add_edge("joker_agent", END)
+    graph.add_node("reviewer_agent", reviewer_agent)
+    graph.add_edge("joker_agent", "reviewer_agent")
+    graph.add_edge("reviewer_agent", END)
     return graph.compile()
 
 
 if __name__ == "__main__":
     agent = build_graph()
-    reponse = agent.invoke({"context": "give me a joke about Coding"})
+    reponse = agent.invoke({"context": "movies"})
     print(reponse)
